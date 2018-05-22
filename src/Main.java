75f5ab8b7f6b43d26acd9684eafc93558d5ab7df
Murat Baskıcıoğlu <muratbaskicioglu@gmail.com>
2018-05-22 23:23:21 +0300

    Add an example of using "ContactsManager"
    An example of using "ContactsManager" class to "Main" class.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String [] args) {
-        System.out.println("Hello world!");
+        ContactsManager myContactManager = new ContactsManager();
+        Contact personOne = new Contact("Murat Baskıcıoğlu", "muratbaskicioglu@gmail.com", "+905354539023");
+//        personOne.name = "Murat Baskıcıoğlu";
+//        personOne.email = "muratbaskicioglu@gmail.com";
+//        personOne.phoneNumber = "+905354539023";
+        myContactManager.addContact(personOne);
+        System.out.println(myContactManager.searchContact("Murat Baskıcıoğlu").phoneNumber);
     }
 }
