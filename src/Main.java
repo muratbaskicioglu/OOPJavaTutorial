012202f9a04c70da40e845ba7ea79a067eb4a369
Murat Baskıcıoğlu <muratbaskicioglu@gmail.com>
2018-05-22 23:49:05 +0300

    An example user inputs with "Scanner" class

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String [] args) {
-        ContactsManager myContactManager = new ContactsManager();
-        Contact personOne = new Contact("Murat Baskıcıoğlu", "muratbaskicioglu@gmail.com", "+905354539023");
-//        personOne.name = "Murat Baskıcıoğlu";
-//        personOne.email = "muratbaskicioglu@gmail.com";
-//        personOne.phoneNumber = "+905354539023";
-        myContactManager.addContact(personOne);
-        System.out.println(myContactManager.searchContact("Murat Baskıcıoğlu").phoneNumber);
+        System.out.println("Enter your name: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
     }
 }
