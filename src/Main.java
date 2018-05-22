bc79f3c88a0ffd25c7278cf7ee56f4bf434dacd5
Murat Baskıcıoğlu <muratbaskicioglu@gmail.com>
2018-05-23 01:56:14 +0300

    Example of serialization and deserialization

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,29 @@
-import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class Main {
     public static void main(String [] args) {
-        int randomNumber = (int) (Math.random() * 100) + 1;
-        boolean hasWon = false;
-
-        System.out.println("I have randomly chosen a number between 1 and 100.");
-        System.out.println("Try to guess it.");
-
-        Scanner scanner = new Scanner(System.in);
-        for(int i = 10; i > 0; i--) {
-            System.out.println("You have " + i + " guess(es) left. Choose again: ");
-            int guess = scanner.nextInt();
-
-            if(randomNumber < guess) {
-                System.out.println("It's smaller than " + guess + ".");
-            } else if(randomNumber > guess) {
-                System.out.println("It's greater than " + guess + ".");
-            } else {
-                hasWon = true;
-                break;
-            }
+        Person p = new Person("Capensis");
+        try {
+            FileOutputStream fos = new FileOutputStream("/home/capensis/Desktop/Udacity Courses/Object Oriented Programming in Java/OOPJavaTutorial/test.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(p);
+            oos.close();
+        } catch(Exception e) {
+            e.printStackTrace();
         }
 
-        if(hasWon) {
-            System.out.println("Correct... You win!");
-        } else {
-            System.out.println("Game over... You lose!");
-            System.out.println("The number was: " + randomNumber);
+        Person getP;
+        try {
+            FileInputStream fis = new FileInputStream("/home/capensis/Desktop/Udacity Courses/Object Oriented Programming in Java/OOPJavaTutorial/test.txt");
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            getP = (Person) ois.readObject();
+            System.out.println(getP.getUserName());
+            ois.close();
+        } catch(Exception e) {
+            e.printStackTrace();
         }
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,17 @@
-public class Person {
+import java.io.Externalizable;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+
+public class Person implements Externalizable {
     private String userName;
     private String SSN;
+
+    public Person() {}
+
+    public Person(String userName) {
+        this.userName = userName;
+        SSN = this.userName.substring(0,2) + ((int) (Math.random() * 10000) + 1);
+    }
 
     private String getId() {
         return SSN + "-" + userName;
@@ -15,6 +26,22 @@ public class Person {
             return true;
         } else {
             return false;
+        }
+    }
+
+    public void writeExternal(ObjectOutput o) {
+        try {
+            o.writeObject(this.userName);
+        } catch(Exception e) {
+            System.out.println("Writing error!");
+        }
+    }
+
+    public void readExternal(ObjectInput o) {
+        try {
+            this.userName = (String) o.readObject();
+        } catch(Exception e) {
+            System.out.println("Reading error!");
         }
     }
 }
