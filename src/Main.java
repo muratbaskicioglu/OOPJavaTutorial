48811e7a997770b0ce5a177a74918311b7189d17
Murat Baskıcıoğlu <muratbaskicioglu@gmail.com>
2018-05-23 00:23:25 +0300

    Simple number guess game

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,32 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String [] args) {
-        System.out.println("Enter your grade: ");
+        int randomNumber = (int) (Math.random() * 100) + 1;
+        boolean hasWon = false;
+
+        System.out.println("I have randomly chosen a number between 1 and 100.");
+        System.out.println("Try to guess it.");
+
         Scanner scanner = new Scanner(System.in);
-        int grade = scanner.nextInt();
-        if(grade > 90) {
-            System.out.println("Wow! You did well!");
-        } else {
-            System.out.println("Not bad, but you can do better next time!");
+        for(int i = 10; i > 0; i--) {
+            System.out.println("You have " + i + " guess(es) left. Choose again: ");
+            int guess = scanner.nextInt();
+
+            if(randomNumber < guess) {
+                System.out.println("It's smaller than " + guess + ".");
+            } else if(randomNumber > guess) {
+                System.out.println("It's greater than " + guess + ".");
+            } else {
+                hasWon = true;
+                break;
+            }
         }
 
+        if(hasWon) {
+            System.out.println("Correct... You win!");
+        } else {
+            System.out.println("Game over... You lose!");
+            System.out.println("The number was: " + randomNumber);
+        }
     }
 }
